09cebfff5b661043450e1b69320d2f1a8f277b57
Felipe Nieves <lfnch@outlook.com>
2024-12-18 18:07:07 -0500

    Creacion de la clase user

--- user/user.js
+++ user/user.js
@@ -1,1 +1,56 @@
 "use strict";
+/**
+ * User
+ * --------------------------------------------------------------------------------------
+ */
+class User{
+  constructor({
+      id = null,
+      document = null,
+      name = null, 
+      password = null, 
+      rol = null, 
+      lastLogin = null, 
+      active = null, 
+    } = {}) {
+      this.id = id;
+      this.document = document;
+      this.name = name;
+      this.password = password;
+      this.rol = rol;
+      this.lastLogin = lastLogin;
+      this.active = active;
+  }
+
+  setRecordSave(document, name, password, rol) {
+    this.document = document;
+    this.name = name;
+    this.password = password;
+    this.rol = rol;
+    this.active = 1;
+  }
+
+  setRecordUpdate(id, document, name, password, rol, active) {
+    this.id = id;
+    this.document = document;
+    this.name = name;
+    this.password = password;
+    this.rol = rol;
+    this.active = active;
+  }
+
+  setRecordUpdateLastLogin(id, lastLogin) {
+    this.id = id;
+    this.lastLogin = lastLogin;
+  }
+
+  getProperties() {
+    return Object.assign({}, this);
+  } 
+
+  isUserObject(object) {
+    if(!(typeof object === 'object' && object !== null && object instanceof User)) {
+      throw new Error("El objecto debe ser una instacia de la clase user.");
+    }
+  } 
+}
